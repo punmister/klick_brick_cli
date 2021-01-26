2907a3d708a625be2b0277cd3a26211530b83612
Prudhvi <poornagurram@gmail.com>
2021-01-26 16:27:34 +0530

    added ci/cd job

--- klick_brick_cli/__init__.py
+++ klick_brick_cli/__init__.py
@@ -1,1 +1,15 @@
 __version__ = '0.1.0'
+
+from argparse import ArgumentParser
+from klick_brick_cli import say_hello
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("hello", help="say hello")
+    parser.add_argument("--name", help="say name")
+    arg = parser.parse_args()
+    if arg.name==None:
+        arg.name = 'None'
+    sample = say_hello.Hello(test_param=[arg.hello, {'name': arg.name}])
+    return sample.say_hello()
